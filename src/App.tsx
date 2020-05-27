9817af589d44841dc81bfb763673d26761f78670
dario888 <dario.bario87@hotmail.com>
2020-05-27 12:38:05 +0200

    Creating: fetchDataAction

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React, {Fragment, useContext} from 'react';
+import React, {Fragment, useContext, useEffect} from 'react';
 
 import {Context} from './Store'
-
+import { FETCH_DATA } from './types'
 
 function App(): JSX.Element {
 
   const {state, dispatch} = useContext(Context)
 
+  useEffect(()=>{
+    fetchDataAction();
+    //eslint-disable-next-line
+  },[])
+
+  const fetchDataAction = async () => {
+    const Url = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
+    const data = await fetch(Url)
+    const dataJson = await data.json()
+    dispatch({type: FETCH_DATA, payload: dataJson._embedded.episodes })
+  }
+
+  console.log(state)
   return (
     <Fragment>
       <h1>Rick and Morty</h1>
